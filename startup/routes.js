a25071f09a44eb8aaf81b9a98916c5d408a32195
Nitish Kumar P S <nitishkumarps997@gmail.com>
2021-08-01 00:49:56 +0530

    default API

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const movies = require('../routes/movies');
 const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const home = require('../routes/home');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
@@ -15,5 +16,6 @@ module.exports = function(app) {
   app.use('/api/rentals', rentals);
   app.use('/api/users', users);
   app.use('/api/auth', auth);
+  app.use("/api/",home)
   app.use(error);
 }
